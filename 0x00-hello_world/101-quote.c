93be9fe9fefd3a7e72c843c0bc26ad4d6f0f61e2
JOHNOKOH <johnokoh735@gmail.com>
2023-02-16 16:36:31 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 /**
  * main - A C program that prints a line to the std error
- * Return: 1 (Success)
+ * Return 1 (Success)
  */
-int main(void)
+int main(void);
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 return (1);
