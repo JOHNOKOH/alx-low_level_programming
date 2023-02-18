178da53640b44a680304e0d5a95dc81fd7e03b49
JOHNOKOH <johnokoh735@gmail.com>
2023-02-18 11:48:20 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry
- * Return: 1 (Success)
+ * main - C program that prints exactly and that piece of art is useful
+ * Return 1 (Success)
  */
-int main(void);
+int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fput(and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
 	return (1);
 }
