0e2576d9943f1729c528303d82d3f6af762477c4
JOHNOKOH <johnokoh735@gmail.com>
2023-02-19 15:25:33 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 /**
- * main - Print followed by new line to standard error
- * Return 1 (Error)
+ * main - Entry point
+ *
+ * Description: A program that print text
+ *
+ * Return 0 (Success)
  */
 int main(void)
 {
-	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, str, strlen(str));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
 	return (1);
 }
