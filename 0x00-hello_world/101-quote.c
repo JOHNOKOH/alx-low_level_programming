3fb13f45efe08d75bbd8d7f78318a6fa57ee1b48
JOHNOKOH <johnokoh735@gmail.com>
2023-02-20 04:40:58 -0800

    Quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,11 @@
  *
  * Description: A program that print text
  *
- * Return 0 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
