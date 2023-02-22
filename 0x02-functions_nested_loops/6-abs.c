e5a50a313fc8d1fe811f2a3cabdb4d597374befb
JOHNOKOH <johnokoh735@gmail.com>
2023-02-21 18:47:32 -0800

    prints last digit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * abs - computes the abs value of an int
+ * abs: - computes the abs value of an int
  * @n: the int in question
  *
- * Return: the abs of an int
+ * Return: the abs of an int.
  */
 
 int _abs(int n);
 {
 	if (n >= 0)
+	{
 		return (n);
-	else
+	}
 		return (-n);
 }
