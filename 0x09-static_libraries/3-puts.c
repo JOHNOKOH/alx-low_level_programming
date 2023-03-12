01060f3b59457f09d0eb28819d7ff9e2e8ccc156
JOHNOKOH <johnokoh735@gmail.com>
2023-03-12 03:28:42 -0700

    newly updated

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,15 +1,10 @@
-#include "main.h"
-
-void _puts(char *str)
-
-#include<stdio.h>
+#include"main.h"
 /**
- * main - Entry point
+ *_puts- the function that change value
+ *@s: the pointer of n
  *
- * Return: Always 0 (Success)
  */
-int main(void)
+void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts(s);
 }
