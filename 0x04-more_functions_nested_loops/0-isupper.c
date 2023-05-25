9ae8f360ca81402c9fc36b703e513565aaa1e8e0
JOHNOKOH <johnokoh735@gmail.com>
2023-05-25 16:17:04 -0700

    checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,15 @@
 /**
  * _isupper - checks for uppercase characters.
  *
- *
  * @c: character to print.
  * Return: 1 if c is upppercase, 0 otherwise.
  */
-
 int _isupper(int c)
 {
-	if (c == 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
 		return (0);
 }
