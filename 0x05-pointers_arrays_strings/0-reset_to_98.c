240de0194cfba01a343fef7ecc7d281a77b931e6
JOHNOKOH <johnokoh735@gmail.com>
2023-05-29 09:02:44 -0700

    prints the value of a pointer to an integer variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer as parameter and updates the value.
+ * @n: input.
+ * Return: Always 0 (success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
