796bb99f4805d2bd8a306dcefc45bc7e26f64e51
JOHNOKOH <johnokoh735@gmail.com>
2023-07-08 12:11:53 -0700

    Token updated

---
+++ 0x05-pointers_arrays_strings/0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - a function that concatenates two strings.
+ * @src: the source string.
+ * @dest: the destination string.
+ * Return: a pointer to the resulting string dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char dest[5] = "Hello";
+	char src [5] = "World";
+	char result;
+
+	(
